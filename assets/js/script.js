fc0a8d738ba50fbbfe6245d37f196299397f42a9
Tyler B Yeager <wow_d2@hotmail.com>
2021-10-05 13:30:18 -0700

    firebase to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,35 @@
 // Play around with edamam search api 
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAVR1qX2b32YafFZ9VcHYOYS6XCF2CMQwc",
+    authDomain: "kitschy-app.firebaseapp.com",
+    projectId: "kitschy-app",
+    storageBucket: "kitschy-app.appspot.com",
+    messagingSenderId: "700644418613",
+    appId: "1:700644418613:web:d5e8843c2e1aa49b6b7d7c",
+    measurementId: "G-C5JM1SY5YH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+      
+
+
+
+
+
+
+
 var foodSearch = "https://api.edamam.com/api/recipes/v2?type=public&q=chicken%20parmesan&app_id=2c66eee1&app_key=102fe174b45e718bfc7022537a02504e";
 var quantity = [];
 
